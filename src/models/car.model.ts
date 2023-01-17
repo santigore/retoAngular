0bce8f1fea83dab64d0568c5af3de1380d63cefe
Santiago Perez <santiagoperez_06@hotmail.com>
2023-01-17 09:07:52 -0500

    Ajuste model

--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -1,8 +1,8 @@
 export interface Car {
     id?: string;
-    name: string;
-    modelo: string;
-    marca: string;
+    name?: string;
+    modelo?: string;
+    marca?: string;
     description?: string;
     photoUrl?: string;
     price?: number;
